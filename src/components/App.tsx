7130b0bb100aaca9bd07c48310fda894b06a3252
Vlad <ksand.1@ya.ru>
2020-07-25 22:43:48 +0400

    init project, react, webPack, ts, tsLint, babel

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../styles/index.css';
+
+class App extends React.PureComponent {
+    render() {
+        return (
+            <div>
+                <h1>Hdde awd1dawd1fawddawd Wdorld!</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
