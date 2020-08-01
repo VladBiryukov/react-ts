3cda90847beb591254db567c7dff875f874a9a24
Vlad <ksand.1@ya.ru>
2020-08-01 12:33:13 +0400

    feature/test branch

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,7 @@ import TestBlock from './blocks/TestBlock';
 
 const App = () : JSX.Element => (
   <div className="app">
-    <h1>Hello. world!</h1>
+    <h1>Hello. world1!</h1>
       <TestBlock/>
   </div>
 );
